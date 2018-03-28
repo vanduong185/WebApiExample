916a90dd99a0ad7ec9dc38c7922939bb438e669d
Van Duong <vanduong18598@gmail.com>
2018-03-28 10:55:43 +0700

    example for webapi-account

---
+++ Webapi/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AccountDataAccess;
+namespace Webapi.Controllers
+{
+    public class AccountController : ApiController
+    {
+        public IEnumerable<account> Get()
+        {
+            using (AccountsEntities entities = new AccountsEntities())
+            {
+                return entities.accounts.ToList();
+            }
+        }
+    }
+}
